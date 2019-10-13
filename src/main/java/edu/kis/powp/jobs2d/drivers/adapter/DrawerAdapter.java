b0f7262ad36da9ae90a5208f690ba851e20fc6e4
unknown <Student@L404-13.carbon.cti.p.lodz.pl>
2019-10-13 15:36:34 +0200

    Reneame Adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerAdapter.java
@@ -9,10 +9,10 @@ import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
  */
-public class MyAdapter extends DrawPanelController implements Job2dDriver {
+public class DrawerAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public MyAdapter() {
+	public DrawerAdapter() {
 		super();
 	}
 
@@ -27,10 +27,10 @@ public class MyAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-
+		DrawerFeature.getDrawerController().drawLine(line);
 		drawLine(line);
 		this.setPosition(x, y);
-		DrawerFeature.getDrawerController().drawLine(line);
+
 	}
 
 	@Override
